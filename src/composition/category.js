925f0bdb77f728567f2735767613751990c059a6
Furs Andrei <you@example.com>
2023-02-28 11:01:14 +0300

    refactoring category js

--- src/composition/category.js
+++ src/composition/category.js
@@ -3,27 +3,29 @@ import axios from 'axios';
 import { useRouter } from "vue-router";
 
 export default function useCategory() {
+    const API_URL = 'http://shopv5/api/categories';
+    
     const categories = ref([]);
     const linkPages = ref([]);
     const category = ref([]);
     const router = useRouter();
     const errors = ref('');
 
-    const getCategories = async (url = 'http://shopv5/api/categories') => {
+    const getCategories = async (url = API_URL) => {
         let response = await axios.get(url);
         categories.value = response.data.data;
         linkPages.value = response.data.meta.links;
     }
     
     const getCategory = async (id) => {
-        let response = await axios.get('http://shopv5/api/categories/' + id);
+        let response = await axios.get(API_URL + '/' + id);
         category.value = response.data;
     }
 
     const storeCategory = async (data) => {
         errors.value = '';
         try {
-            await axios.post('http://shopv5/api/categories', data);
+            await axios.post(API_URL, data);
             await router.push({ name: 'category.index' });
         } catch(e) {
             //построчный вывод ошибок
@@ -37,7 +39,7 @@ export default function useCategory() {
         errors.value = '';
         
         try {
-            await axios.put('http://shopv5/api/categories/' + id, category.value);
+            await axios.put(API_URL + '/' + id, category.value);
             await router.push({ name: 'category.index' });
         } catch(e) {
             //построчный вывод ошибок
@@ -50,7 +52,7 @@ export default function useCategory() {
     const destroyCategory = async (id) => {
         try {
             clearErrors();
-            await axios.delete('http://shopv5/api/categories/'+id);
+            await axios.delete(API_URL + '/' + id);
         } catch(e) {
             //вывод ошибок
             if (e.response.status === 409) {
